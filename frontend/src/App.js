0ba77ee4e675a8f8396328f40dbe86ba53f5ba18
ryanmckim <mck649@gmail.com>
2023-10-27 20:26:31 -0700

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/members")
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setData(data);
+  //     });
+  // }, []);
+
+  return (
+    <div>
+      {/* {data.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        data.map((member, i) => (
+          <p key={i}>{member}</p>
+        ))
+      )} */}
+    </div>
+  );
+}
